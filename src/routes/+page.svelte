1e9ce51dc9a666fb27deee08337d801ae0b9e240
royid_poulose <royidcp96@gmail.com>
2024-01-17 13:42:55 +0530

    Admin login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to KnowCom</h1>
 <a href="/Admin">Admin</a><br>
 <a href="/Admin/Products">Products</a>
-<a href="/Admin/Dashboard">Dashboard</a>
+<a href="Dashboard/Admin/">Dashboard</a>

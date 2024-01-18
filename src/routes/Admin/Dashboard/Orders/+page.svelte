ab48605b2123bce21b7e991519b36e4d479082ab
royid_poulose <royidcp96@gmail.com>
2024-01-18 17:15:00 +0530

    Orders table created

--- src/routes/Admin/Dashboard/Orders/+page.svelte
+++ src/routes/Admin/Dashboard/Orders/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import type { PageData } from '../../$types';
-    
+    import DataTable from "./orderTable.svelte";
+
     export let data: PageData;
 </script>
 <p>Orders</p>
+<div class="container mx-auto py-10">
+    <DataTable />
+</div>

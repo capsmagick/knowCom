46ba5dc820eb51ce57bef0e184b70153bc47dfe6
royid_poulose <royidcp96@gmail.com>
2024-01-18 12:56:46 +0530

    categoryTable

--- src/routes/Admin/Dashboard/Categories/+page.svelte
+++ src/routes/Admin/Dashboard/Categories/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import type { PageData } from '../../$types';
-    
+    import DataTable from "./categorieTable.svelte";
     export let data: PageData;
-</script>
-<p>Categories</p>
+  </script>
+  <p>Categories</p>
+  <div class="container mx-auto py-10">
+    <DataTable />
+  </div>
+

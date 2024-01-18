50c997e19bc0acd35dd3360c8d4acccc7e9c5972
royid_poulose <royidcp96@gmail.com>
2024-01-18 18:16:22 +0530

    Customer table added

--- src/routes/Admin/Dashboard/Customers/+page.svelte
+++ src/routes/Admin/Dashboard/Customers/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import type { PageData } from '../../$types';
-    
+    import DataTable from "./customerTable.svelte";
+
     export let data: PageData;
 </script>
-<p>customer</p>
+<p>Customer</p>
+<div class="container mx-auto py-10">
+    <DataTable />
+</div>
